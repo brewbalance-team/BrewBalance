8e714ebbe0410deffcb780cbebdc497f0bf81c20
Joan <joanzhu728@gmail.com>
2026-02-08 14:45:41 +0900

    fix: move app icon to public and update paths to /brand-icon.png

--- components/CreativeLogo.tsx
+++ components/CreativeLogo.tsx
@@ -4,7 +4,7 @@ const CreativeLogo: React.FC = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <img
-        src="/Default app icon.png"
+        src="/brand-icon.png"
         alt="BrewBalance Logo"
         className="w-full h-full object-cover rounded-xl"
       />
